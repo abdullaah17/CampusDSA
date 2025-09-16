1144b9fc2cdf1e66305b2a1ce933943cbd5d1a23
abdullah17 <bachelorabdullah17@gmail.com>
2025-09-17 04:25:30 +0500

    Initial commit

--- Arrays/Lab1Q3.cpp
+++ Arrays/Lab1Q3.cpp
@@ -4,8 +4,8 @@ using namespace std;
 class ArrayAnalyzer{
     int arr[5];
     int n;
-    int Largest = arr[0];
-    int SecLargest = arr[0];
+    int Largest;
+    int SecLargest;
 
     public:
     ArrayAnalyzer(){
@@ -15,19 +15,37 @@ class ArrayAnalyzer{
     for(int i=0; i<n; i++){
         cin>>arr[i];
     }
+    Largest = arr[0];
+    SecLargest = arr[0];
     }
     int findSecondLargest(){
+        Largest = arr[0];
+        SecLargest = arr[0];
+
         for(int i=0; i<n; i++){
             if(arr[i]>Largest){
                 SecLargest = Largest;
                 Largest = arr[i];
             }
-            else if(arr[i]>SecLargest){
+            else if(arr[i]>SecLargest && arr[i]<Largest){
                 SecLargest = arr[i];
             }
         }
-        cout<<"Second Largest:"<<SecLargest;
-
+        cout<<"Second Largest:"<<SecLargest<<endl;
 
     }
+    void display() {
+        if (Largest == SecLargest) {
+            cout << "Second Largest does not exist" << endl;
+        } else {
+            cout << "Second Largest = " << SecLargest << endl;
+        }
+    }
+};
+
+int main(){
+    ArrayAnalyzer A1;
+    A1.findSecondLargest();
+    A1.display();
+    return 0;
 }
