3f8d987d575695decbe870031712aa23a6926a25
abdullah <bachelorabdullah17@gmail.com>
2025-12-25 02:03:44 +0500

    Delete backendFocused directory

--- backendFocused/Async in FastAPI.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI #Python framework for building backend APIs.
-import asyncio
-
-app = FastAPI()
-
-@app.get("/data")
-async def get_data():
-    await asyncio.sleep(2)
-    return {"status": "ok"}
